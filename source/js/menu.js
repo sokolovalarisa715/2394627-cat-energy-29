2c5ac742cd41a3ce26347e8862046ac4791d56b4
sokolovalarisa715 <sokolovalarisa715@gmail.com>
2023-09-17 18:28:50 +0300

    Сделала Npm Lint

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,8 +1,8 @@
-const menu = document.querySelector(".main-nav__wrapper")
-  , menuToggle = document.querySelector(".burger-menu__toggle")
-  , menuButton = document.querySelector(".burger-menu__toggle")
+const menu = document.querySelector(".navigation__wrapper")
+  , menuToggle = document.querySelector(".navigation__toggle")
+  , menuButton = document.querySelector(".navigation__toggle")
   , menuActiveClass = "main-nav__wrapper--active"
-  , menuToggleActiveClass = "burger-menu__toggle--active";
+  , menuToggleActiveClass = "navigation__toggle--active";
 menuButton.addEventListener("click", (function () {
   menuToggle.classList.toggle(menuToggleActiveClass),
     menu.classList.toggle(menuActiveClass)
